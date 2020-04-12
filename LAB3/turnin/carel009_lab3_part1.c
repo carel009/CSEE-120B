110a06458c35bec5b6334a2904a39510cbf4c2ce
CaliDobie <43662514+CaliDobie@users.noreply.github.com>
2020-04-11 20:01:35 -0700

    Update carel009_lab3_part1.c

--- LAB3/turnin/carel009_lab3_part1.c
+++ LAB3/turnin/carel009_lab3_part1.c
@@ -1,6 +1,6 @@
-/*	Author: lab
+/*	Author: Christopher Arellano
  *  Partner(s) Name: 
- *	Lab Section:
+ *	Lab Section: 026
  *	Assignment: Lab #3  Exercise #1
  *	Exercise Description: [optional - include for your own benefit]
  *
